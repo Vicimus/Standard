902055c35b14b73e36f59ec9bb8f257006bcafd3
Jordan Grieve <quintilex@gmail.com>
2019-04-04 14:40:23 -0400

    new requirements

---
+++ VicimusStandard/Sniffs/Whitespace/BlankLinesSniff.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Vicimus\Standard\Sniffs\Whitespace;
+
+use PHP_CodeSniffer\Standards\Squiz\Sniffs\WhiteSpace\SuperfluousWhitespaceSniff;
+
+/**
+ * Class BlankLinesSniff
+ */
+class BlankLinesSniff extends SuperfluousWhitespaceSniff
+{
+    //
+}
